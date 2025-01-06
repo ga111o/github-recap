87505b82ba126b39b6b089cee6edbb2812762ed8
ga111o <ga111o@proton.me>
2025-01-06 14:14:28 +0900

    private 레포도 함께 저장해요

--- be/modules/get_user_repos.py
+++ be/modules/get_user_repos.py
@@ -11,7 +11,8 @@ dotenv.load_dotenv()
 def get_user_repos(token: str, start_date: datetime, end_date: datetime) -> List[Dict] | tuple:
     """
     start_date, end_date 사이에 있는 모든 레포(레포이름, url, private유무, 최종업뎃날짜) 가져옴
-
+    private 레포지토리도 포함
+    
     params: Dict[str, str] = {
       'start_date': start_date.strftime('%Y-%m-%d'),
       'end_date': end_date.strftime('%Y-%m-%d')
@@ -22,10 +23,17 @@ def get_user_repos(token: str, start_date: datetime, end_date: datetime) -> List
 
     url: str = "https://api.github.com/user/repos"
     headers: Dict[str, str] = {
-        'Authorization': f'token {token}'
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
     }
     
-    response: requests.Response = requests.get(url, headers=headers)
+    params: Dict[str, str] = {
+        'visibility': 'all',  # public, private 모두 포함
+        'sort': 'updated',
+        'per_page': 100
+    }
+    
+    response: requests.Response = requests.get(url, headers=headers, params=params)
     
     if response.status_code == 200:
         repos: List[Dict] = response.json()
@@ -55,5 +63,4 @@ if __name__ == "__main__":
             updated_at: str = repo['updated_at']
             
             ic(repo_name, repo_url, is_private, updated_at)
-    else:
-        ic(repos)
+
