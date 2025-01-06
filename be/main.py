dcbf524c7ac6a8a01ca488e547ae01798d44156a
ga111o <ga111o@proton.me>
2025-01-06 11:55:21 +0900

    get active days, streak, gap, each_day_commit_count 얻는 api

--- be/main.py
+++ be/main.py
@@ -13,7 +13,11 @@ from .modules import (
     validate_date_n_token,
     get_total_commit_num, 
     get_specific_repo_commit_num,
-    get_active_days
+    get_active_days,
+    get_longest_streak,
+    get_longest_gap,
+    get_total_days,
+    get_each_day_commit_count
 )
 
 @asynccontextmanager
@@ -139,13 +143,51 @@ async def get_used_language(
 
     return get_used_language(user, start_date, end_date)
 
-@app.get("/get/{user}/days/active")
-async def get_active_days(
+@app.get("/get/{user}/days/active/{year}/{month}")
+async def get_active_days_endpoint(
     user: str,
-    year: Optional[int] = None,
-    month: Optional[int] = None,
+    year: int,
+    month: int,
     github_token: Optional[str] = Header(None, alias="X-GitHub-Token")
 ):
-    active_days, total_days = get_active_days(user, github_token, year, month)
-    return {"active_days": active_days, "total_days": total_days}
+    active_days = get_active_days(user, github_token, year, month)
+    return {"active_days": active_days}
 
+@app.get("/get/{user}/days/longest_streak/{year}/{month}")
+async def get_longest_streak_endpoint(
+    user: str,
+    year: int,
+    month: int,
+    github_token: Optional[str] = Header(None, alias="X-GitHub-Token")
+):
+    longest_streak = get_longest_streak(user, github_token, year, month)
+    return {"longest_streak": longest_streak}
+
+@app.get("/get/{user}/days/longest_gap/{year}/{month}/{day}")
+async def get_longest_gap_endpoint(
+    user: str,
+    year: int,
+    month: int,
+    day: int,
+    github_token: Optional[str] = Header(None, alias="X-GitHub-Token")
+):
+    longest_gap = get_longest_gap(user, github_token, year, month, day)
+    return {"longest_gap": longest_gap}
+
+@app.get("/get/days/total/{year}/{month}")
+async def get_total_days_endpoint(
+    year: int,
+    month: int,
+):
+    total_days = get_total_days(year, month)
+    return {"total_days": total_days}
+
+@app.get("/get/{user}/days/each/{year}/{month}")
+async def get_each_day_commit_count_endpoint(
+    user: str,
+    year: int,
+    month: int,
+    github_token: Optional[str] = Header(None, alias="X-GitHub-Token")
+):
+    each_day_commit_count = get_each_day_commit_count(user, github_token, year, month)
+    return {"each_day_commit_count": each_day_commit_count}
